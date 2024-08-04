2696f3426be2b3e765a9a7f6e24d26d65a090033
Hannah Ahn <hannahjahn1024@gmail.com>
2024-08-04 10:26:04 +0900

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,53 @@
+'use client'
+import Image from "next/image";
+import {useState, useEffect} from 'react'
+import {firestore} from '@/firebase'
+import { Box, Typography } from "@mui/material";
+import { collection, getDoc, getDocs, query } from 'firebase/firestore'
+
+export default function Home() {
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState('');
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      })
+    })
+    setInventory(inventoryList)
+  }
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()) {
+      const {quantity} = docSnap.data()
+    }
+  }
+
+  useEffect(() => {
+    updateInventory()
+  }, [])
+
+  return (
+    <Box>
+      <Typography variant="h1">Inventory Management</Typography>
+      {inventory.forEach((item) => {
+        console.log(item)
+        return(
+        <Box>
+          {item.name}
+          {item.count}
+        </Box>
+        )
+      })}
+    </Box>
+  );
+}
